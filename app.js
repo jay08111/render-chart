f8a5d61a388d0708a56325f7a3fbe0e79fb705cb
hoyeoun <jay0i8111@naver.com>
2022-12-11 15:58:48 +0900

    # ADD

--- app.js
+++ app.js
@@ -317,3 +317,62 @@ const drawChart3 = async () => {
 };
 
 drawChart3();
+
+const drawChart4 = () => {
+  const mockData = [
+    { name: "simon", score: 80 },
+    { name: "Mary", score: 90 },
+    { name: "John", score: 60 },
+    { name: "jay", score: 89 },
+  ];
+
+  const width = 800;
+  const height = 400;
+  const margin = {
+    top: 50,
+    bottom: 50,
+    left: 50,
+    right: 50,
+  };
+  const svg = d3
+    .select("#chart_3")
+    .append("svg")
+    .attr("width", width - margin.left - margin.right)
+    .attr("height", height - margin.top - margin.bottom)
+    .attr("viewBox", [0, 0, width, height]);
+
+  const x = d3
+    .scaleBand()
+    .domain(d3.range(mockData.length))
+    .range([margin.left, width - margin.right])
+    .padding(0.1);
+  const y = d3
+    .scaleLinear()
+    .domain([0, 100])
+    .range([height - margin.bottom, margin.top]);
+
+  svg
+    .append("g")
+    .attr("fill", "royalblue")
+    .selectAll("rect")
+    .data(mockData.sort((a, b) => d3.descending(a.score, b.score)))
+    .join("rect")
+    .attr("x", (d, i) => x(i))
+    .attr("y", (d) => y(d.score))
+    .attr("height", (d) => y(0) - y(d.score))
+    .attr("width", x.bandwidth());
+  const xAxis = (g) => {
+    g.attr("transform", `translate(0 , ${height - margin.bottom})`)
+      .call(d3.axisBottom(x).tickFormat((i) => mockData[i].name))
+      .attr("font-size", "20px");
+  };
+  const yAxis = (g) => {
+    g.attr("transform", `translate(${margin.left} , 0)`)
+      .call(d3.axisLeft(y).ticks(null, mockData.format))
+      .attr("font-size", "20px");
+  };
+  svg.append("g").call(xAxis);
+  svg.append("g").call(yAxis);
+};
+
+drawChart4();
